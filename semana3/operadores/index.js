6a83cce1fa08178eb999c1a8b668140375e983ff
felipemimoura <felipemimoura@gmail.com>
2020-12-01 13:27:28 -0300

    Conclusão exercicios

---
+++ semana3/operadores/index.js
@@ -0,0 +1,49 @@
+/* Exercício de Interpretação */
+
+// 1 - a) false
+//1 - b) false
+//1 - c) true
+//1 - d) boolean
+
+//2 - a) undefined
+//2 - b) null
+//2 - c) 11
+//2 - d) 3
+//2 - e) [3,19,5,6,7,8,9,10,11,12,13]
+//2 - f) 9
+
+/* Exercicios de escrita de código */
+//Exercício 01
+/* let idadeUsuario = prompt('Qual a sua idade?')
+let idadeAmigo = prompt('Qual a idade do seu melhor amigo(a)?')
+let maiorIdade = idadeUsuario > idadeAmigo 
+let quemEMaisVelho = prompt('Sua idade é maior do que a do seu amigo(a)?') 
+alert(maiorIdade)
+let diferencaIdade = idadeUsuario - idadeAmigo
+console.log(`A diferença de idade entre você e seu amigo é de ${diferencaIdade} anos`) */
+
+//Exercício 2
+/* let numeroPar = prompt('Digite um número par')
+let verificarNumeroPar = Number(numeroPar) % 2
+console.log(verificarNumeroPar) */
+
+//Todos os números pares o resultado é igual a zero
+//Quando digitamos um número impar o resultado é 1
+
+//Exercício 3
+/* let listaDeTarefas = []
+let tarefa1 = prompt('Digite uma tarefa a ser executado')
+listaDeTarefas.push(tarefa1)
+let tarefa2 = prompt('Digite outra tarefa a ser executado')
+listaDeTarefas.push(tarefa2)
+let tarefa3 = prompt('Digite mais uma tarefa a ser executado')
+listaDeTarefas.push(tarefa3)
+console.log(listaDeTarefas)
+let tarefaExecutado = Number(prompt('Digite o indice da tarefa que você concluiu'))
+listaDeTarefas.splice(tarefaExecutado, tarefaExecutado)
+console.log(listaDeTarefas) */
+
+//Exercicio 4
+/* let nomeDoUsuario = prompt('Qual é o seu nome?')
+let emailDoUsuario = prompt('Qual o seu email')
+alert(`O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vindo ${nomeDoUsuario}`) */
