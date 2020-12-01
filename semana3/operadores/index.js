bd47214a34656aa45116e932253ea9b9cfac1c6e
felipemimoura <felipemimoura@gmail.com>
2020-12-01 14:05:12 -0300

    Retirando comentarios

--- semana3/operadores/index.js
+++ semana3/operadores/index.js
@@ -14,24 +14,24 @@
 
 /* Exercicios de escrita de código */
 //Exercício 01
-/* let idadeUsuario = prompt('Qual a sua idade?')
+let idadeUsuario = prompt('Qual a sua idade?')
 let idadeAmigo = prompt('Qual a idade do seu melhor amigo(a)?')
 let maiorIdade = idadeUsuario > idadeAmigo 
 let quemEMaisVelho = prompt('Sua idade é maior do que a do seu amigo(a)?') 
 alert(maiorIdade)
 let diferencaIdade = idadeUsuario - idadeAmigo
-console.log(`A diferença de idade entre você e seu amigo é de ${diferencaIdade} anos`) */
+console.log(`A diferença de idade entre você e seu amigo é de ${diferencaIdade} anos`) 
 
 //Exercício 2
-/* let numeroPar = prompt('Digite um número par')
+let numeroPar = prompt('Digite um número par')
 let verificarNumeroPar = Number(numeroPar) % 2
-console.log(verificarNumeroPar) */
+console.log(verificarNumeroPar)
 
 //Todos os números pares o resultado é igual a zero
 //Quando digitamos um número impar o resultado é 1
 
 //Exercício 3
-/* let listaDeTarefas = []
+let listaDeTarefas = []
 let tarefa1 = prompt('Digite uma tarefa a ser executado')
 listaDeTarefas.push(tarefa1)
 let tarefa2 = prompt('Digite outra tarefa a ser executado')
@@ -41,27 +41,27 @@ listaDeTarefas.push(tarefa3)
 console.log(listaDeTarefas)
 let tarefaExecutado = Number(prompt('Digite o indice da tarefa que você concluiu'))
 listaDeTarefas.splice(tarefaExecutado, tarefaExecutado)
-console.log(listaDeTarefas) */
+console.log(listaDeTarefas) 
 
 //Exercicio 4
-/* let nomeDoUsuario = prompt('Qual é o seu nome?')
+let nomeDoUsuario = prompt('Qual é o seu nome?')
 let emailDoUsuario = prompt('Qual o seu email')
-alert(`O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vindo ${nomeDoUsuario}`) */
+alert(`O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vindo ${nomeDoUsuario}`) 
 
 //Desafios  1
 
 //a. Calcule e mostre o valor de 77°F em  K, mostrando a unidade no console também.
-/* const conversaoFehrenheitParaKelvin = ((77 - 32) * (5/9)) + 273.15
+const conversaoFehrenheitParaKelvin = ((77 - 32) * (5/9)) + 273.15
 const conversaoCelcsiusParaFahenheit = (80 * (9/5)) + 32
 const conversaoCelcsiusParaKelvin = 80 + 273.15
 console.log( `A conversão de 77º F em kelvin ${conversaoFehrenheitParaKelvin}k`)
 //b. Calcule e mostre o valor de 80°C em °F, mostrando a unidade no console também.
 console.log( `A conversão de 80ºC em Fahrenheit ${conversaoCelcsiusParaFahenheit}F`)
 //c. Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também.
-console.log(`A conversão de 30ºC para kelvin é ${conversaoCelcsiusParaKelvin}K e para Fahenheit ${conversaoCelcsiusParaFahenheit}F`) */
+console.log(`A conversão de 30ºC para kelvin é ${conversaoCelcsiusParaKelvin}K e para Fahenheit ${conversaoCelcsiusParaFahenheit}F`)
 
 //Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter.
-/* let temperatura = Number(prompt('Digite uma temperatura em graus Celsius'))
+let temperatura = Number(prompt('Digite uma temperatura em graus Celsius'))
 const conversaoFehrenheitParaKelvin = ((temperatura - 32) * (5/9)) + 273.15
 const conversaoCelcsiusParaFahenheit = (temperatura * (9/5)) + 32
 const conversaoCelcsiusParaKelvin = temperatura + 273.15
@@ -69,14 +69,14 @@ console.log( `A conversão de ${temperatura} F em kelvin ${conversaoFehrenheitPa
 //b. Calcule e mostre o valor de 80°C em °F, mostrando a unidade no console também.
 console.log( `A conversão de ${temperatura} em Fahrenheit ${conversaoCelcsiusParaFahenheit}F`)
 //c. Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também.
-console.log(`A conversão de ${temperatura} para kelvin é ${conversaoCelcsiusParaKelvin}K e para Fahenheit ${conversaoCelcsiusParaFahenheit}F`) */
+console.log(`A conversão de ${temperatura} para kelvin é ${conversaoCelcsiusParaKelvin}K e para Fahenheit ${conversaoCelcsiusParaFahenheit}F`) 
 
 //Desafio 2
-/* const custoQuilowatt = 0.05
+const custoQuilowatt = 0.05
 const desconto = 0.85
 let consumo = Number(prompt('Digite seu consumo de energia'))
 console.log(`O valor a ser pago é de R$ ${consumo * custoQuilowatt},00 `)
-console.log(`O valor a ser pago com desconto é de R$ ${(consumo * custoQuilowatt) * desconto} `) */
+console.log(`O valor a ser pago com desconto é de R$ ${(consumo * custoQuilowatt) * desconto} `) 
 
 
 //Desafio 03
