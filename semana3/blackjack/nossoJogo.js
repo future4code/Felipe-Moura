7f07c6094b84a2a982092a1ae7bb033811da9b67
felipemimoura <felipemimoura@gmail.com>
2020-12-04 13:31:35 -0300

    Projeto blackjack concluido

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,3 +10,44 @@
  * 
  * 
  */
+
+let pontos = 0
+let primeiraCarta = comprarCarta()
+let segundaCarta = comprarCarta()
+let primeiraCartaComputador = comprarCarta()
+let segundaCartaComputador = comprarCarta()
+
+console.log('Bem vindo ao jogo de Blackjack')
+if(confirm('Quer comprar uma nova carta')){
+   primeiraCarta.texto
+   pontos = primeiraCarta.valor
+   segundaCarta.texto
+   pontos += segundaCarta.valor
+   let pontosUsuario = pontos
+   pontos = 0
+   primeiraCartaComputador.texto
+   
+   pontos = primeiraCartaComputador.valor
+   segundaCartaComputador.texto
+   pontos += segundaCartaComputador.valor
+   let pontosComputador = pontos
+   
+   if(pontosUsuario <= 21 && pontosComputador <= 21){
+      console.log(`O Usuario - Cartas ${primeiraCarta.texto} ${segundaCarta.texto} pontuação ${pontosUsuario}`)
+      console.log(`O Computador - Cartas ${primeiraCartaComputador.texto} ${segundaCartaComputador.texto} pontuação ${pontosComputador}`)
+
+   }
+   if(pontosComputador === pontosUsuario){
+      console.log('Deu empate')
+   }else if(pontosUsuario > pontosComputador){
+      console.log('Usuario ganhou')
+   }else{
+      console.log('Computador Ganhou')
+   }
+ 
+  
+  
+ 
+}else{
+   console.log(`O jogo acabou`)
+}
