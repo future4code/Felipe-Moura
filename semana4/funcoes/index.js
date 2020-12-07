20d59f9a0631b11308870af00de57d9985f04594
felipemimoura <felipemimoura@gmail.com>
2020-12-07 11:22:32 -0300

    Resolução do primeiro exérciciop de interpretação de código

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,11 @@
+//Exercícios de interpretação de códigos
+
+//Exercício 1
+//A) Vai ser impresso no console o valor de 10 e o valor 20
+//B) A função vai ser excutada porém não será exibida nada no console
+
+
+
+
+
+
