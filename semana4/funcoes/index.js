b138fdca3c3d8856afdca7d697671e37be1644dc
felipemimoura <felipemimoura@gmail.com>
2020-12-07 11:28:29 -0300

    Resolução do segundo exercicio de interpretação de código

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -4,6 +4,10 @@
 //A) Vai ser impresso no console o valor de 10 e o valor 20
 //B) A função vai ser excutada porém não será exibida nada no console
 
+//Exercício 2
+//A) No console vai ser somente dois elementos do array
+//B) Seria impresso somente os dois nomes
+
 
 
 
