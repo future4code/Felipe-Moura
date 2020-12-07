1b0987311068003d5c2fd9c99f848160e647bb25
felipemimoura <felipemimoura@gmail.com>
2020-12-07 11:52:11 -0300

    Resolsução do primeiro exercicos de escrita de código

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -12,7 +12,34 @@
 /* A função metodo recebe um array de números como parâmetro, verifica se os números desses array são pares em casos deles serem pares irá realizar a multiplicação do número por ele mesmo e enviar uma novo array. Um novo nome poderia ser: "potenciaDeNumerosPares".
 */
 
+//Exercicios de escrita de código
+//Exercicio 4
+/*  function escreveSobreMim(){
+ let nome = 'Felipe'
+  let idade = 26
+  let local = 'São Paulo'
+  let profissao = 'Estudante'
 
+  return console.log(`Eu sou o ${nome}, tenho ${idade}, moro em ${local} e sou ${profissao}`)
+}
 
+escreveSobreMim() */
 
+function sobreMim(nome, idade, endereco, simOuNao){
+  
+  return console.log(`Eu sou o ${nome}, tenho ${idade}, moro em ${endereco} e ${simOuNao} estudante`)
+}
 
+let nome = prompt('Digite o seu nome');
+let idade = Number(prompt('Quantos anos você possui?'))
+let endereco = prompt('Qual o seu endereço?')
+let estudante = prompt("Você é estudante?, Responda Sim ou Não").toLowerCase()
+let simOuNao = ""
+console.log(estudante)
+if(estudante === 'sim'){
+  simOuNao = 'sou'
+} else{
+  simOuNao = 'não sou'
+}
+
+sobreMim(nome, idade, endereco, simOuNao)
