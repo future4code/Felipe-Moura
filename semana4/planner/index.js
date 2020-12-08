4c068a9ef8d59edad78543bed1c1466cc315fea1
felipemimoura <felipemimoura@gmail.com>
2020-12-08 16:01:35 -0300

    Realização da Responsivdade

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,48 +1,17 @@
-const domingo = document.getElementById('domingo')
-const segunda = document.getElementById('segunda')
-const terca = document.getElementById('terca')
-const quarta = document.getElementById('quarta')
-const quinta = document.getElementById('quinta')
-const sexta = document.getElementById('sexta')
-const sabado = document.getElementById('sabado')
-
-
 function addTask(){
   const tarefa = document.getElementById('tarefa')
   const diaDaSemana = document.getElementById('dias-semana')
-  let tarafaValor = tarefa.value
   let diaDaTarefa = diaDaSemana.value
- 
+  let tarefaValor = tarefa.value
+  let adicionaTarefa = document.getElementById(`${diaDaTarefa}`)
 
-  switch(diaDaTarefa){
-    case 'domingo':
-      domingo.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
-    case 'segunda':
-      segunda.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
-    case 'terca':
-      terca.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
-    case 'quarta':
-      quarta.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
-    case 'quinta':
-      quinta.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
-    case 'sexta':
-      sexta.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
-    case 'sabado':
-      sabado.innerHTML += `<ul><li>${tarafaValor}</li></ul>`
-      tarefa.value = ''
-    break
+  if(tarefaValor){
+    adicionaTarefa.innerHTML += `<p class='to-do'>${tarefaValor} </p> `
+  }else{
+    alert('Insira uma tarefa')
   }
+  tarefa.value = ''
 }
+
+
 
