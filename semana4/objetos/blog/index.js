d0239e0aed7bd62e5d6c5dcd3885181be85a5267
felipemimoura <felipemimoura@gmail.com>
2020-12-09 14:27:18 -0300

    Realização do exercico da tarde

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,8 +1,22 @@
+const container = document.getElementById("container-de-posts")
+const tituloPost = document.getElementById('titulo-post')
+const autorPost = document.getElementById('autor-post')
+const conteudoPost = document.getElementById('conteudo-post')
+
+
+
 function adicionaPost(){
-  const container = document.getElementById("container-de-posts")
-  const tituloPost = document.getElementById('titulo-post')
-  const autorPost = document.getElementById('autor-post')
-  const conteudoPost = document.getElementById('conteudo-post')
-  container.innerHTML += `<div class="novo-post"> <h1 class="titulo">${tituloPost.value}</h1><h3 class="autor">${autorPost.value}</h3><p class="conteudo">${conteudoPost.value}</p></div>`
-  console.log(tituloPost.value)
+  const conteudo = {
+    titulo: tituloPost.value,
+    nomeAutor: autorPost.value,
+    postagem: conteudoPost.value
+  }
+  container.innerHTML += `<div class="novo-post"> <h1 class="titulo">${conteudo.titulo}</h1><h3 class="autor">${conteudo.nomeAutor}</h3><p class="conteudo">${conteudo.postagem}</p></div>`
+  
+
+  
+}
+
+function criarPost(event){
+  adicionaPost()
 }
