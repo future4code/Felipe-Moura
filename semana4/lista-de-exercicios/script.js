07b3ec8ec73dd9662eb7f2a9b2881c03b2da9c61
felipemimoura <felipemimoura@gmail.com>
2020-12-11 10:48:25 -0300

    Resolução do primeiro exericico de interpretação de código

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,7 @@
+//Exercicio de Interpretação de código
+/* 1) É uma conversão de moeda, convertendo o valor em dolares para reais. 
+  Essa função espera recever um valor que será convertido.
+  Pede para o usuario a contação de um dolar (R$ 5,074 no dia 11/12/2020),
+  realiza a multiplicação do valor da cotação(5.074) pelo valor a ser convertido nesse caso 100
+  Nesse caso retornar R$ 507,40 
+*/
