4d1203841e9e88fef9df427fe9570f66a65d20f7
felipemimoura <felipemimoura@gmail.com>
2020-12-11 11:08:05 -0300

    Resolução do segundo exercicio de interpretação de código

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -5,3 +5,12 @@
   realiza a multiplicação do valor da cotação(5.074) pelo valor a ser convertido nesse caso 100
   Nesse caso retornar R$ 507,40 
 */
+
+/* 2)Essa função espera receber dois parâmetros sendo o primeiro o tipo de investimento e o segundo o valor
+  para cada tipo de investimento o valor será multiplicado por coefiente já estimulado.
+  Caso o tipo do investimento não seja encontrado vai ser mostrar uma mensagem na tela.
+  Após a verificação do tipo do investimento ela retorna o valor atual do investimento.
+
+  No console será exibito R$ 165,00 para o investimenmto em ações
+  No segundo montante será exibido o alerta de 'Tipo de investimento informado incorreto' pois o investimento em tesouro direto não está disponível
+*/
