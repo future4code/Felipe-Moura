920cb92cf8d088020ed0181bfc8ceb6f63451ac9
felipemimoura <felipemimoura@gmail.com>
2020-12-17 16:23:08 -0300

    Resolução da primeira atividade

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,24 +3,44 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    informacoesPost:[
+      {
+        nomeUsuario: "Paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150"
+      },
+      {
+        nomeUsuario: "Felipe",
+        fotoUsuario: "https://picsum.photos/200/300",
+        fotoPost: "https://picsum.photos/220/150"
+      } ,
+      {
+        nomeUsuario: "Labenu_",
+        fotoUsuario: "https://picsum.photos/55/50",
+        fotoPost: "https://picsum.photos/240/150"
+      },
+    ]
+  } 
+
+  
+  
+  
   render() {
+    const listaDePost = this.state.informacoesPost.map((postagem) =>{
+      return(
+        <Post 
+        nomeUsuario = {postagem.nomeUsuario}
+        fotoUsuario = {postagem.fotoUsuario}
+        fotoPost = {postagem.fotoPost}
+        
+        />
+      )
+    })
+    
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Felipe'}
-          fotoUsuario={'https://picsum.photos/200/300'}
-          fotoPost={'https://picsum.photos/210/150'}
-        />
-        <Post
-          nomeUsuario={'Labenu_'}
-          fotoUsuario={'https://picsum.photos/55/50'}
-          fotoPost={'https://picsum.photos/240/150'}
-        />
+        {listaDePost}
       </div>
     );
   }
