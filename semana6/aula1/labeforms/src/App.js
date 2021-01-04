fa3c7e02b5f8723661f6695269f622026862ae6f
Felipe Moura <felipemimoura@gmail.com>
2021-01-04 12:02:56 -0300

    Criação dos arquivos inciais e da primeira etapa

---
+++ semana6/aula1/labeforms/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Etapa 1 - Dados Gerais </h1>
+      <p>1. Qual o seu Nome?</p>
+      <input type="text>"/>
+
+      <p>2. Qual a sua idade?</p>
+      <input type="number"/>
+
+      <p>3. Qual o seu Email?</p>
+      <input type="email"/>
+
+      <p>4. Qual a sua escolaridade</p>
+      <select>
+        <option value="ensinoMedioCompleto">Ensino Medio Completo</option>
+        <option value="ensinoMedioIncompleto">Ensino Medio Incompleto</option>
+        <option value="ensinoSuperiorCompleto">Ensino Superior Completo</option>
+        <option value="ensinoSuperiorIncompleto">Ensino Superior Incompleto</option>
+      </select>
+      <button>Próxima Etapa</button>
+    </div>
+  );
+}
+
+export default App;
