523454d52a16e4a228d704b53f7486b6912114e1
Felipe Moura <felipemimoura@gmail.com>
2021-01-15 15:48:32 -0300

    Logica de adicionar musicas na playlist

--- semana7/projeto/labefy/src/pages/ViewPlaylist.js
+++ semana7/projeto/labefy/src/pages/ViewPlaylist.js
@@ -1,51 +1,98 @@
-import React from 'react'
-import {baseURL, axiosHeaders} from '../params'
-import axios from 'axios'
+import React from "react";
+import { baseURL, axiosHeaders } from "../params";
+import axios from "axios";
+import { AddMusic } from "./AddMusic";
 
-export class ViewPlaylist extends React.Component{
-    state = {
-        list: []
+export class ViewPlaylist extends React.Component {
+  state = {
+    list: [],
+    showAddMusic: false,
+    playlistName: "",
+    playlistId: ""
+  };
+  componentDidMount() {
+    this.getPlaylist();
+  }
+  componentDidUpdate() {
+    this.getPlaylist();
+  }
+  getPlaylist = async () => {
+    try {
+      const response = await axios.get(baseURL, axiosHeaders);
+      // console.log(response.data.result.list)
+      this.setState({
+        list: response.data.result.list,
+      });
+      // console.log(this.state.list)
+    } catch (err) {
+      console.log(err);
     }
-    componentDidMount(){
-        this.getPlaylist()
-    }
-    componentDidUpdate(){
-        this.getPlaylist()
-    }
-    getPlaylist =  async () =>{
-        try{
-            const response = await axios.get(baseURL, axiosHeaders)
-            // console.log(response.data.result.list)
-            this.setState({
-                list: response.data.result.list
-            })
-            // console.log(this.state.list)
-        }
-        catch(err){
-            console.log(err)
-        }
-    }
+  };
 
-    deletePlaylist = (id) =>{
-        axios.delete(`${baseURL}/${id}`, axiosHeaders)
-            .then((res) => {
-                   console.log(res) 
-                   this.getPlaylist()
-            })
-            .catch((err) =>{
-                console.log(err)
-            })
-    }
-    render(){
-        const playlist = this.state.list.map((playlist) =>{
-            return(
-                <li key={playlist.id}>{playlist.name} 
-                    <button onClick={() => {this.deletePlaylist(playlist.id)}}> X</button>
-                </li>
-            )
-        })
-        return(
-            <ul>{playlist}</ul>
-        )
-    }
+  deletePlaylist = (id) => {
+    axios
+      .delete(`${baseURL}/${id}`, axiosHeaders)
+      .then((res) => {
+        console.log(res);
+        this.getPlaylist();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  addMusic = (name, id) => {
+    axios
+      .get(`${baseURL}/search?name=${name}`, axiosHeaders)
+      .then((res) => {
+        // console.log(res);
+        this.setState({
+          showAddMusic: !this.state.showAddMusic,
+          playlistName: name,
+          playlistId: id
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const playlist = this.state.list.map((playlist) => {
+      return (
+        <span>
+          <li key={playlist.id}>
+            {playlist.name}
+            <button
+              onClick={() => {
+                this.addMusic(playlist.name, playlist.id);
+              }}
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                this.deletePlaylist(playlist.id);
+              }}
+            >
+              X
+            </button>
+            <button
+              onClick={() => {
+                this.showMusic(playlist.name, playlist.id);
+              }}
+            >
+              Ver Musicas 
+            </button>
+          </li>
+        </span>
+      );
+    });
+    return (
+      <div>
+        <ul>{playlist}</ul>
+        {this.state.showAddMusic && <AddMusic addMusicToPlaylist={this.state.playlistName} playlistId={this.state.playlistId}/>}
+        
+      </div>
+    );
+  }
 }
