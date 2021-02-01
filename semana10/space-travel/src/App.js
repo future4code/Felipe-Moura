edb8620b3d08ce0a2d588b3cac5e260d9760b188
Felipe Moura <felipemimoura@gmail.com>
2021-02-01 14:11:43 -0300

    Criação da estrutura da home

---
+++ semana10/space-travel/src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div>
+
+      <h1>Bem Vindo a LabeX</h1>
+      <p>
+        Sua próxima viagem espacial está aqui.
+      </p>
+
+      <h2>Quem é você?</h2>
+
+      <button>Viajante</button>
+      <button>Organizador</button>
+    </div>
+  );
+}
+
+export default App;
