a8616ca2ed3dc8f04662e86bf500eca7f1f2db05
felipemimoura <felipemimoura@gmail.com>
2021-02-22 15:04:37 -0300

    criaçao da logica de cadastro

--- semana11/lab-eddit/src/Components/Login/LoginCreate.js
+++ semana11/lab-eddit/src/Components/Login/LoginCreate.js
@@ -1,9 +1,38 @@
+import axios from 'axios'
 import React from 'react'
+import Input from '../../Components/Forms/Input'
+import useForm from '../../hooks/useForm'
+import Button from '../Forms/Button'
 
 const LoginCreate = () => {
+    const userName = useForm()
+    const email = useForm()
+    const password = useForm()
+    
+    const handleSubmit = (event) =>{
+        const body = {
+            email: email.value,
+            password: password.value,
+            username: userName.value
+
+        }
+        axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/signup',body
+            
+        ).then((res) =>{
+            console.log(res)
+        }).catch(err =>{
+            console.log(err)
+        })
+    }
+
     return (
         <div>
-            teste
+            <h2>Cadastra-se</h2>
+            <Input label="Username" type="text" name="username" {...userName} />
+            <Input label="Email" type="email" name="email" {...email}/>
+            <Input label="Senha" type="password" name="password" {...password} />
+
+            <Button onClick={handleSubmit}>Cadastrar</Button>
         </div>
     )
 }
