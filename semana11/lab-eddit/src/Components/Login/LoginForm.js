0b107cfa63393bc98a341907932692a0652dc2f1
felipemimoura <felipemimoura@gmail.com>
2021-02-22 15:20:52 -0300

    criaçao da logica de login

--- semana11/lab-eddit/src/Components/Login/LoginForm.js
+++ semana11/lab-eddit/src/Components/Login/LoginForm.js
@@ -4,11 +4,25 @@ import useForm from '../../hooks/useForm'
 import Button from '../Forms/Button'
 import Input from '../Forms/Input'
 import {goToCreatePage} from '../../Coordinator'
+import axios from 'axios'
 
 function LoginForm() {
     const email = useForm()
     const password = useForm()
     const history = useHistory()
+
+    const handleSubmit = () => {
+        const body ={
+            email: email.value,
+            password: password.value
+
+        }
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labEddit/login", body).then(res =>{
+            console.log(res)
+        }).catch(err =>{
+            console.log(err)
+        })
+    }
 
 
     return (
@@ -16,7 +30,7 @@ function LoginForm() {
             <h1>Login</h1>
             <Input label="Email" type="email" name="email"{...email} />
             <Input label="senha" type="password" name="password" {...password} />
-            <Button>Entrar</Button>
+            <Button onClick={handleSubmit}>Entrar</Button>
             <Button onClick={() => goToCreatePage(history)}>Cadastra-se</Button>
         </div>
     )
