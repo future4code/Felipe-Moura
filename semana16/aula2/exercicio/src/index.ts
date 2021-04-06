db141c6589eea16f261a590ce02d628cca161c93
Felipe Moura <felipemoura@DESKTOP-Q6PMQI4.localdomain>
2021-04-06 12:39:54 -0300

    criação da função de geração de um hash e comprativo desses hash

--- semana16/aula2/exercicio/src/index.ts
+++ semana16/aula2/exercicio/src/index.ts
@@ -2,8 +2,20 @@ import app from "./app";
 import { createUser } from "./endpoints/createUser";
 import { login } from "./endpoints/login";
 import { singupUser } from "./endpoints/singup";
+import { compareHash, generateHash } from "./services/generateHash";
+
+
+const hash = async(str:string) =>{
+    const hash =  await generateHash(str)
+    const compare = await compareHash(str, hash)
+    console.log(hash)
+    console.log(compare)
+    return compare
+}
+
+hash('str')
+hash('stra')
 
 app.post('/user/create', createUser)
-
 app.post('/user/singup', singupUser)
 app.post('/user/login', login)
